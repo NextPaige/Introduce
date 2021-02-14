8da1be750d315457556c73282cd12503b4a504ee
inandexhale95 <inandexhale95@gmail.com>
2021-02-14 23:25:14 +0900

    초기 커밋.

---
+++ Intro.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Introduce
+{
+    /// <summary>
+    /// Intro.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Intro : Page
+    {
+        DispatcherTimer timer;
+
+        int counter = 0;
+        int length = 0;
+        string text;
+
+        public Intro()
+        {
+            timer = new DispatcherTimer();
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Interval = TimeSpan.FromMilliseconds(100);
+            timer.Start();
+            InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            text = txt_Intro.Text;
+            length = txt_Intro.Text.Length;
+            txt_Intro.Text = string.Empty;
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            txt_Intro.Text = text.Substring(0, counter);
+            counter++;
+
+            if (counter > length)
+            {
+                timer.Stop();
+            }
+        }
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Introduce
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            left_Frame.Content = new Intro();
+            
+        }
+
+        private void grid_menu_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void btn_Home_Click(object sender, RoutedEventArgs e)
+        {
+            left_Frame.Content = new Home();
+        }
+
+        private void btn_Contact_Click(object sender, RoutedEventArgs e)
+        {
+            left_Frame.Content = new Contact();
+        }
+
+        private void btn_WindowMini_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btn_WindowClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void btn_Github_Click(object sender, RoutedEventArgs e)
+        {
+            var url = new ProcessStartInfo
+            {
+                UseShellExecute = true,
+                FileName = "https://github.com/NextPaige"
+            };
+            Process.Start(url);
+        }
+    }
+}
